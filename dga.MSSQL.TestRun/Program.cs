4c2f369e17028795718970e78745c6b7775a2710
alien28 <prakarn@gmail.com>
2020-01-18 21:13:37 +0700

    test commit3

--- dga.MSSQL.TestRun/Program.cs
+++ dga.MSSQL.TestRun/Program.cs
@@ -7,6 +7,7 @@ namespace dga.MSSQL.TestRun
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!!");
+            Console.WriteLine("test commit to repo GitHub1");
         }
     }
 }
